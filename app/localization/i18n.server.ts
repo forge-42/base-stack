7a337b79c3fc5e75b3659c95d581f0c90480ee8e
rphlmr <raphael@uaerom.com>
2025-04-18 09:43:24 +0200

    quick fix

--- app/localization/i18n.server.ts
+++ app/localization/i18n.server.ts
@@ -2,17 +2,18 @@ import { RemixI18Next } from "remix-i18next/server"
 import i18n from "~/localization/i18n" // your i18n configuration file
 import { resources } from "./resource"
 
-const i18next = new RemixI18Next({
-	detection: {
-		supportedLanguages: i18n.supportedLngs,
-		fallbackLanguage: i18n.fallbackLng,
-	},
-	// This is the configuration for i18next used
-	// when translating messages server-side only
-	i18next: {
-		...i18n,
-		resources,
-	},
-})
+const i18next = (fallbackLanguage?: string) =>
+	new RemixI18Next({
+		detection: {
+			supportedLanguages: i18n.supportedLngs,
+			fallbackLanguage: fallbackLanguage || "en",
+		},
+		// This is the configuration for i18next used
+		// when translating messages server-side only
+		i18next: {
+			...i18n,
+			resources,
+		},
+	})
 
 export default i18next
