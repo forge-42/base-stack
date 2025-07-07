e256003a1ee913196e28afb1ace1d05caa60351e
Alem Tuzlak <t.zlak@hotmail.com>
2025-07-07 10:54:24 +0200

    Update biome to v2, zod to v4, all other deps as well

--- app/routes/sitemap.$lang[.]xml.ts
+++ app/routes/sitemap.$lang[.]xml.ts
@@ -5,12 +5,12 @@ import type { Route } from "./+types/sitemap.$lang[.]xml"
 export const loader = async ({ request, params }: Route.LoaderArgs) => {
 	const domain = createDomain(request)
 
-	// @ts-expect-error - This import exists but is not picked up by the typescript compiler because it's a remix internal
 	const { routes } = await import("virtual:react-router/server-build")
 
 	const sitemap = await generateRemixSitemap({
 		domain,
-		routes,
+		// biome-ignore lint/suspicious/noExplicitAny: The types in the library are wrong
+		routes: routes as any,
 		ignore: ["/resource/*"],
 		// Transforms the url before adding it to the sitemap
 		urlTransformer: (url) => `${url}?lng=${params.lang}`,
