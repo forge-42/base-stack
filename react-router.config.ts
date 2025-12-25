55395245b3655ffe2a1fe97c269c1f7fceaa85eb
Alem Tuzlak <t.zlak@hotmail.com>
2025-12-25 12:18:28 +0100

    Feat/update deps (#58)
    Fixes #
    
    # Description
    
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context.
    List any dependencies that are required for this change.
    
    ## Type of change
    
    Please mark relevant options with an `x` in the brackets.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    - [ ] Algorithm update - updates algorithm
    documentation/questions/answers etc.
    - [ ] Other (please describe):
    
    # How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also
    list any relevant details for your test configuration
    
    - [ ] Integration tests
    - [ ] Unit tests
    - [ ] Manual tests
    - [ ] No tests required
    
    # Reviewer checklist
    
    Mark everything that needs to be checked before merging the PR.
    
    - [ ] Check if the UI is working as expected and is satisfactory
    - [ ] Check if the code is well documented
    - [ ] Check if the behavior is what is expected
    - [ ] Check if the code is well tested
    - [ ] Check if the code is readable and well formatted
    - [ ] Additional checks (document below if any)
    
    # Screenshots (if appropriate):
    
    # Questions (if appropriate):

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "@react-router/dev/config"
 
 export default {
 	future: {
-		unstable_viteEnvironmentApi: true,
-		unstable_splitRouteModules: true,
-		unstable_optimizeDeps: true,
+		v8_viteEnvironmentApi: true,
+		v8_splitRouteModules: true,
 		v8_middleware: true,
+		unstable_optimizeDeps: true,
 	},
 } satisfies Config
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import { playwright } from "@vitest/browser-playwright"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
@@ -7,10 +8,43 @@ export default defineConfig({
 		globals: true,
 		css: true,
 		coverage: {
-			all: false,
 			include: ["app/**"],
 			reporter: ["text", "json-summary", "json"],
 			reportOnFailure: true,
 		},
+		projects: [
+			{
+				extends: true,
+				test: {
+					name: "server tests",
+					environment: "node",
+					// Include generic .test files that should work anywhere and .server.test files for server only, ignore .browser.test
+					include: ["./**/*.server.test.{ts,tsx}", "!./**/*.browser.test.{ts,tsx}", "./**/*.test.{ts,tsx}"],
+				},
+			},
+			{
+				extends: true,
+				optimizeDeps: {
+					include: ["react/jsx-dev-runtime"],
+				},
+				server: {
+					fs: {
+						strict: false,
+					},
+				},
+				test: {
+					includeTaskLocation: true,
+					// Include generic .test files that should work anywhere and .browser.test files for browser only, ignore .server.test
+					include: ["./**/*.test.{ts,tsx}", "./**/*.browser.test.{ts,tsx}", "!./**/*.server.test.{ts,tsx}"],
+					setupFiles: ["./tests/setup.browser.tsx"],
+					name: "browser tests",
+					browser: {
+						enabled: true,
+						instances: [{ browser: "chromium" }],
+						provider: playwright(),
+					},
+				},
+			},
+		],
 	},
 })
